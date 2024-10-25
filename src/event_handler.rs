ed13bc6d5658ebe33642709962ca41b971387534
Logan Lucas <logan.lucas83@gmail.com>
2024-10-25 17:31:45 +0200

    handle file moving

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,35 +1,21 @@
-use std::{fs, path::{Path, PathBuf}};
-use notify::{event::CreateKind, Event, EventKind};
+use std::path::Path;
+use notify::{event::{CreateKind, ModifyKind, RenameMode}, Event, EventKind};
 
 use crate::file_utils;
 
 pub fn handle_event<P: AsRef<Path>>(event: Event, path: &P) -> () {
     match event.kind {
+        // Maybe check if file is already in correct dir?
         EventKind::Create(create_kind) => {
             if let CreateKind::File = create_kind {
-                println!("New file created at {:?}", event.paths);
-                // I am guessing there can only be one path in Event::paths if creating a file
-                let file_path = &event.paths[0];
-                let extension = match file_path.extension() {
-                    Some(ext) => ext.to_str().unwrap(),
-                    None => "none",
-                };
-
-                if let Some(destination_dir) = file_utils::EXTENSION_TO_DIRECTORY
-                    .lock()
-                    .unwrap()
-                    .get(extension)
-                {
-                    let mut path_buffer = PathBuf::new();
-                    path_buffer.push(path);
-                    path_buffer.push(destination_dir);
-                    if fs::create_dir_all(&path_buffer).is_ok() {
-                        path_buffer.push(file_path.file_name().unwrap());
-                        if let Err(e) = fs::rename(&event.paths[0], &path_buffer) {
-                            eprintln!("Failed to move file: {}", e);
-                        }
-                    }
-                }
+                println!("New file created at {:?}", event.paths[0]);
+                file_utils::move_to_correct_dir(&event.paths[0], path);
+            }
+        },
+        EventKind::Modify(modify_kind) => {
+            if let ModifyKind::Name(RenameMode::To) = modify_kind {
+                println!("File moved at {:?}", event.paths[0]);
+                file_utils::move_to_correct_dir(&event.paths[0], path);
             }
         },
         _ => {
--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,7 +1,7 @@
-use std::{collections::HashMap, sync::Mutex};
+use std::{collections::HashMap, fs, path::{Path, PathBuf}, sync::Mutex};
 use once_cell::sync::Lazy;
 
-pub static EXTENSION_TO_DIRECTORY: Lazy<Mutex<HashMap<&str, &str>>> = Lazy::new(|| {
+static EXTENSION_TO_DIRECTORY: Lazy<Mutex<HashMap<&str, &str>>> = Lazy::new(|| {
     let mut map = HashMap::new();
     map.insert("pdf", "pdf documents");
     map.insert("cpp", "C++ source code");
@@ -11,3 +11,26 @@ pub static EXTENSION_TO_DIRECTORY: Lazy<Mutex<HashMap<&str, &str>>> = Lazy::new(
 
     return Mutex::new(map);
 });
+
+pub fn move_to_correct_dir<P: AsRef<Path>>(file_path: &PathBuf, organized_path: &P) -> () {
+    let extension = match file_path.extension() {
+        Some(ext) => ext.to_str().unwrap(),
+        None => "none",
+    };
+
+    if let Some(destination_dir) = EXTENSION_TO_DIRECTORY
+        .lock()
+        .unwrap()
+        .get(extension)
+    {
+        let mut path_buffer = PathBuf::new();
+        path_buffer.push(organized_path);
+        path_buffer.push(destination_dir);
+        if fs::create_dir_all(&path_buffer).is_ok() {
+            path_buffer.push(file_path.file_name().unwrap());
+            if let Err(e) = fs::rename(file_path, &path_buffer) {
+                eprintln!("Failed to move file: {}", e);
+            }
+        }
+    }
+}
