bcb0c87e9f6dd81b2cf2fb7d581015bad36b5c60
Logan Lucas <logan.lucas83@gmail.com>
2024-10-27 18:34:38 +0100

    create organized directory on launch if it does not exist

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod event_watcher;
 mod event_handler;
 mod file_utils;
 
+use std::{fs, process::exit};
+
 fn main() {
     let watched_path = std::env::args()
         .nth(1)
@@ -12,9 +14,15 @@ fn main() {
     println!("Watching {}", watched_path);
     println!("Organizing to {}", destination_path);
 
+    if let Err(e) = fs::create_dir_all(&destination_path) {
+        println!("[Error] Could not create directory {}", destination_path);
+        println!("error: {:?}", e);
+        exit(1);
+    }
+
     futures::executor::block_on(async {
         if let Err(e) = event_watcher::async_watch(watched_path, destination_path).await {
-            println!("error: {:?}", e)
+            println!("error: {:?}", e); // TODO: better error display for users
         }
     });
 }
